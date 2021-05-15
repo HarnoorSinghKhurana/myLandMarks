5354a40550c199bd95f7b3167b102ef5e298fb63
Harnoor Singh <harnoors@nvidia.com>
2021-05-15 13:34:50 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Harnoor Singh on 15/05/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
